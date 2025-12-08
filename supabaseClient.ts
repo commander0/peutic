5721fbe8a86d5d4fecd7f4ecb95c5e3648d0ecd6
commander0 <Djbauer1993@gmail.com>
2025-12-07 17:25:48 -0800

    Delete supabaseClient.ts

--- supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Your provided credentials
-const supabaseUrl = 'https://qdnctbupmlqhzubwigjn.supabase.co';
-const supabaseKey = 'sb_publishable_FF7py8rGlouj6dGj32TZpg_5bQa7i4g';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
